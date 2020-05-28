e25f13e07c4d278ee50b081c6ed8508b582a2fd3
htactive-vinhnt <vinhnt@htactive.com>
2020-05-28 13:33:39 +0700

    mme

---
+++ slider.js
@@ -0,0 +1,21 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var images = document.getElementsByClassName("image");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < images.length; i++) {
+    images[i].className = images[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  images[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 10000);
+}
