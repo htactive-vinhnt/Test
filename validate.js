534ab1e605f3c230e2e1a865f58e03a5aeec77a8
htactive-vinhnt <vinhnt@htactive.com>
2020-06-01 18:36:45 +0700

    third-update

--- validate.js
+++ validate.js
@@ -1,19 +1,24 @@
 function validate() {
-  var name = document.forms["contactForm"]["i-name"].value;
+  var Iname = document.forms["contactForm"]["i-name"].value;
   var mail = document.forms["contactForm"]["i-email"].value;
   var subj = document.forms["contactForm"]["i-subject"].value;
   var message = document.forms["contactForm"]["i-message"].value;
-  if (name == "") {
-    alert("Tên không được để trống");
+
+  if (Iname == "") {
+    alert("name không được để trống");
   }
   if (mail == "") {
     alert("mail không được để trống");
-    
   }
   if (subj == "") {
     alert("subj không được để trống");
   }
   if (message == "") {
     alert("message không được để trống");
+  } else {
+    document.getElementById("showName").innerHTML = Iname;
+    document.getElementById("showEmail").innerHTML = mail;
+    document.getElementById("showSubject").innerHTML = subj;
+    document.getElementById("showMessage").innerHTML = message;
   }
 }
